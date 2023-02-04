97774bb0552d587970399a2f596e3e6fbccd7eba
Ronaldo Bicca <kuka@MacBook-Pro-de-Ronaldo.local>
2023-02-04 10:50:07 -0300

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,41 @@
+<script>
+  import { onMount } from "svelte";
+  import Home from "./lib/pages/Home.svelte";
+  import Settings from "./lib/pages/Settings.svelte";
+
+  let page;
+
+  const onRouteChange = () => {
+    const path = window.location.hash.slice(1);
+    if (path === '/') {
+      page = 'home';
+    } else if (path === '/settings') {
+      page = 'settings';
+    } else {
+      window.location.hash = '/';
+    }
+  };
+
+  onMount(onRouteChange);
+
+</script>
+
+<svelte:window  on:hashchange={onRouteChange} />
+
+<h1>The Store</h1>
+<nav>
+  <a href="#/">Home</a>
+  <a href="#/settings">Settings</a>
+</nav>
+
+{#if page === 'home'}
+  <Home />
+{:else if page === 'settings'}
+  <Settings />
+{ /if}
+
+
+
+<style>
+  
+</style>
---
+++ src/lib/pages/Home.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onDestroy } from "svelte";
+    import settings from "../stores/settings";
+
+    let colorScheme;
+
+    const unsubscribe = settings.subscribe( val => {
+        colorScheme = val.colorScheme;
+    });
+
+    onDestroy(unsubscribe);
+    
+</script>
+
+<h2>Home</h2>
+{colorScheme}
